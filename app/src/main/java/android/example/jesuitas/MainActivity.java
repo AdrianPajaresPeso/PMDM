6961c80c59a85955b9b77f2b63b20b1b96b3d999
Adrián <dam08.2020.jesuitas@gmail.com>
2020-10-15 20:29:05 +0200

    develop / add text and imageView

--- app/src/main/java/android/example/jesuitas/MainActivity.java
+++ app/src/main/java/android/example/jesuitas/MainActivity.java
@@ -3,6 +3,8 @@ package android.example.jesuitas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void likeItMsg(View view) {
+        Toast like = Toast.makeText(this, R.string.msgLikeIt, Toast.LENGTH_LONG );
+        like.show();
     }
 }
