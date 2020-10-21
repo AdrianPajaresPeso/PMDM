175fb7fecb4d73614f73181b009209201474e809
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-21 10:49:44 +0200

    develop / A.1.9. comenzada

--- app/src/main/java/android/example/jesuitas/MainActivity.java
+++ app/src/main/java/android/example/jesuitas/MainActivity.java
@@ -2,21 +2,55 @@ package android.example.jesuitas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int numberOfProducts = 0;
+    private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.numeroQuantity);
     }
 
-    public void likeItMsg(View view) {
-        Toast like = Toast.makeText(this, R.string.msgLikeIt, Toast.LENGTH_LONG );
-        like.show();
+
+    public void makeOrder(View view) {
+
+        Intent intent = new Intent(this, Activity2.class);
+        CheckBox nata  = findViewById(R.id.whippedCream);
+        CheckBox chocolate  = findViewById(R.id.chocolate);
+        TextView numeroCafes = (TextView) findViewById(R.id.numeroQuantity);
+        ;
+        double price = (double) Integer.parseInt(String.valueOf(numeroCafes.getText()))*1.50;
+
+        if(nata.isChecked()){
+            price = price + 0.5;
+        }
+        if(chocolate.isChecked()){
+            price = price + 0.5;
+        }
+        
+
+        //intent.putExtra("")
+        startActivity(intent);
     }
 
+    public void countUp(View view) {
+        ++numberOfProducts;
+        if ( mShowCount!= null) {
+            mShowCount.setText(Integer.toString(numberOfProducts));
+        }
+    }
+    public void countDown(View view) {
+        ++numberOfProducts;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(numberOfProducts));
+        }
+    }
 }
