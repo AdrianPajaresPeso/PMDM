b1024a39d1b4e0168d36640f8f4fd5d06c459de2
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-29 12:07:08 +0100

    develop / reset Workspace

--- app/src/androidTest/java/android/example/implicitintentsreceiver/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/android/example/implicitintentsreceiver/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.implicityintent;
+package android.example.implicitintentsreceiver;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.implicityintent", appContext.getPackageName());
+        assertEquals("android.example.implicitintentsreceiver", appContext.getPackageName());
     }
 }
