739fb3b3a72d816ffbef45d63f17d803fcdd60d7
Adrián <dam08.2020.jesuitas@gmail.com>
2020-10-30 07:17:37 +0100

    release / bugfix and more dataControl

--- app/src/main/java/android/example/e12pedidosconintents/MainActivity.java
+++ app/src/main/java/android/example/e12pedidosconintents/MainActivity.java
@@ -27,63 +27,75 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Metodo que se encarga de recoger toda la información
      * de la actividad principal y pasarla a la actividad secundaria
-     * */
+     */
     public void makeOrder(View view) {
-
-        //rescursos que se van a utilizar
-        Intent intent = new Intent(this, Activity2.class);
-        CheckBox nata  = findViewById(R.id.whippedCream);
-        CheckBox chocolate  = findViewById(R.id.chocolate);
+        CheckBox nata = findViewById(R.id.whippedCream);
+        CheckBox chocolate = findViewById(R.id.chocolate);
         TextView x = (TextView) findViewById(R.id.numeroQuantity);
-
-        //valores para calculo
-        Integer numeroCafes = Integer.parseInt(String.valueOf(x.getText()));
-        double price = (double) numeroCafes*1.50;
         TextView nombre = (TextView) findViewById(R.id.inputName);
         TextView e_mail = findViewById(R.id.editTextTextEmailAddress);
 
-        //si la nata está checada se le añade 0.5 por café pedido
-        if(nata.isChecked()){
-            price += (0.5*numeroCafes);
+
+        // si el numero de productos es 0 no podemos continuar
+        if(numberOfProducts == 0){
+            Toast.makeText(this, R.string.toastPreOrder0, Toast.LENGTH_SHORT).show();
+        }else if(nombre.getText().toString().equals("")  || e_mail.getText().toString().equals("")){
+            Toast.makeText(this, R.string.toastPreOrder1, Toast.LENGTH_SHORT).show();
         }
-        //si el chocolate está checada se le añade 0.5 por cafe pedido
-        if(chocolate.isChecked()){
-            price += (0.5*numeroCafes);
-        }
+        else{
 
-        //se define el string dependiendo de los toppings elegidos
-        String productos;
-        if(nata.isChecked() && chocolate.isChecked()){
-            productos= getString(R.string.withCreamAndChocolate);
-        }else if(nata.isChecked()){
-            productos = getString(R.string.withCream);
-        }else if(chocolate.isChecked()){
-            productos = getString(R.string.withChocolate);
-        }else{
-            if(numeroCafes>1){
-                productos =getString(R.string.without);
+            //rescursos que se van a utilizar
+            Intent intent = new Intent(this, Activity2.class);
 
-            }else{
-                productos =getString(R.string.without_single);
 
+            //valores para calculo
+            Integer numeroCafes = Integer.parseInt(String.valueOf(x.getText()));
+            double price = (double) numeroCafes * 1.50;
+
+
+            //si la nata está checada se le añade 0.5 por café pedido
+            if (nata.isChecked()) {
+                price += (0.5 * numeroCafes);
             }
-        }
+            //si el chocolate está checada se le añade 0.5 por cafe pedido
+            if (chocolate.isChecked()) {
+                price += (0.5 * numeroCafes);
+            }
 
-        //se preparan los datos y se pasan a la siguiente actividad
-        intent.putExtra("EXTRA_EMAIL",e_mail.getText().toString());
-        intent.putExtra("EXTRA_PRODUCTS", productos);
-        intent.putExtra("EXTRA_NAME", String.valueOf(nombre.getText()));
-        intent.putExtra("EXTRA_PRICE", String.valueOf(price));
-        intent.putExtra("EXTRA_NUMBER", String.valueOf(numeroCafes));
-        startActivity(intent);
+            //se define el string dependiendo de los toppings elegidos
+            String productos;
+            if (nata.isChecked() && chocolate.isChecked()) {
+                productos = getString(R.string.withCreamAndChocolate);
+            } else if (nata.isChecked()) {
+                productos = getString(R.string.withCream);
+            } else if (chocolate.isChecked()) {
+                productos = getString(R.string.withChocolate);
+            } else {
+                if (numeroCafes > 1) {
+                    productos = getString(R.string.without);
+
+                } else {
+                    productos = getString(R.string.without_single);
+
+                }
+            }
+
+            //se preparan los datos y se pasan a la siguiente actividad
+            intent.putExtra("EXTRA_EMAIL", e_mail.getText().toString());
+            intent.putExtra("EXTRA_PRODUCTS", productos);
+            intent.putExtra("EXTRA_NAME", String.valueOf(nombre.getText()));
+            intent.putExtra("EXTRA_PRICE", String.valueOf(price));
+            intent.putExtra("EXTRA_NUMBER", String.valueOf(numeroCafes));
+            startActivity(intent);
+        }
     }
 
     /**
      * Funcion que hace que el contador de cafes suba
-     * */
+     */
     public void countUp(View view) {
         ++numberOfProducts;
-        if ( mShowCount!= null) {
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(numberOfProducts));
         }
     }
@@ -91,11 +103,14 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Funcion que hace que el contador de cafes baje
-     * */
+     */
     public void countDown(View view) {
-        --numberOfProducts;
-        if (mShowCount != null) {
-            mShowCount.setText(Integer.toString(numberOfProducts));
+        //solo restará cafes si el numero es mayor a 0
+        if (numberOfProducts > 0) {
+            --numberOfProducts;
+            if (mShowCount != null) {
+                mShowCount.setText(Integer.toString(numberOfProducts));
+            }
         }
     }
 }
