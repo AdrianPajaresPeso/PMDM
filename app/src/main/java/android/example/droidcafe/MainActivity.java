8e565517b01bb0afe47f23a746180898dfe49e4a
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-11-18 10:42:33 +0100

    develop / icono aniadido

--- app/src/main/java/android/example/droidcafe/MainActivity.java
+++ app/src/main/java/android/example/droidcafe/MainActivity.java
@@ -1,6 +1,5 @@
 package android.example.droidcafe;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,9 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,32 +28,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onClick(View view) {
-        Intent intent = new Intent(this, OrderActivity.class);
-        startActivity(intent);
-    }
-
 }
