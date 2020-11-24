3732dfdeb0b31c450f6a9fa9b171cbfd579d07a6
Adrián <dam08.2020.jesuitas@gmail.com>
2020-11-24 20:37:25 +0100

    A.2.5 Terminado y testeado

--- app/src/main/java/android/example/droidcafe/MainActivity.java
+++ app/src/main/java/android/example/droidcafe/MainActivity.java
@@ -2,9 +2,12 @@ package android.example.droidcafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,12 +17,54 @@ public class MainActivity extends AppCompatActivity {
     private String mOrderMessage;
     public static final String EXTRA_MESSAGE =
             "com.example.android.droidcafe.extra.MESSAGE";
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                
+                if(mOrderMessage == null){
+                    displayToast("There is not product selected");
+                }else{
+                    Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                    intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                    startActivity(intent);
+
+                }
+
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do nothing
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
