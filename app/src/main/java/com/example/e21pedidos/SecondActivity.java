2f9cfef28e1c61e7ea42ea070001ae7ed7eb0cf8
Adrián <dam08.2020.jesuitas@gmail.com>
2020-11-25 19:58:25 +0100

    develop / preparacion para E.2.1

---
+++ app/src/main/java/com/example/e21pedidos/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.e21pedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
---
+++ app/src/main/java/com/example/e21pedidos/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.example.e21pedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
