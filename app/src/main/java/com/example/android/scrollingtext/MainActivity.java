29acd3aa1cd3d0a9b7e58921b4e21259569c8515
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-11-25 11:10:13 +0100

    develop / A.2.6 bisTris

--- app/src/main/java/com/example/android/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/android/scrollingtext/MainActivity.java
@@ -18,23 +18,112 @@ package com.example.android.scrollingtext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActionMode aMode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //aqui se define que objeto se queda clicado para el contextual
         TextView article_text = findViewById(R.id.article);
         registerForContextMenu(article_text);
+
+
+        //contextual Action Bar
+        TextView subcabecera = findViewById(R.id.article_subheading);
+        subcabecera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              if(aMode!= null){
+                  return;
+              }
+              aMode = MainActivity.this.startActionMode(aModeCallBack);
+              view.setSelected(true);
+            }
+
+        });
+
+
+        Button mButton = findViewById(R.id.comment_button);
+        mButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(MainActivity.this,view);
+                popup.getMenuInflater().inflate(R.menu.menu_context,popup.getMenu());
+                popup.show();
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.context_edit:
+                                displayToast("Edit choice clicked.");
+                                return true;
+                            case R.id.context_share:
+                                displayToast("Share choice clicked.");
+                                return true;
+                            case R.id.context_delete:
+                                displayToast("Delete choice clicked.");
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+            }
+        });
     }
+    private ActionMode.Callback aModeCallBack = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.context_action_bar,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.context_edit:
+                    displayToast("Edit choice clicked.");
+                    aMode.finish();
+                    return true;
+                case R.id.context_share:
+                    displayToast("Share choice clicked.");
+                    aMode.finish();
+                    return true;
+                case R.id.context_delete:
+                    displayToast("Delete choice clicked.");
+                    aMode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            aMode = null;
+        }
+    };
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
