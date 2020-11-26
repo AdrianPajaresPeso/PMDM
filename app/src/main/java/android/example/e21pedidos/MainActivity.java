a15a7c42234f83af697fc2cb5b83f7e554734c9c
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-11-26 13:44:44 +0100

    develop / toolbar done

--- app/src/main/java/android/example/e21pedidos/MainActivity.java
+++ app/src/main/java/android/example/e21pedidos/MainActivity.java
@@ -1,8 +1,12 @@
 package android.example.e21pedidos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =  findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
     }
 }
