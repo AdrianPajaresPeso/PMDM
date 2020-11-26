9cf7e73ddbc15d014c223dec983cdaeab96630bd
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-11-26 12:59:15 +0100

    reimport project

---
+++ app/src/main/java/android/example/e21pedidos/MainActivity.java
@@ -0,0 +1,14 @@
+package android.example.e21pedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
