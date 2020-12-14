181084fdbdee4f895bbe6b70144015f256668826
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-12-14 10:40:34 +0100

    recyclerview / adapter creado

--- app/src/main/java/android/example/recyclerview/WordListAdapter.java
+++ app/src/main/java/android/example/recyclerview/WordListAdapter.java
@@ -1,39 +1,62 @@
 package android.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
-    @NonNull
-    @Override
-    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+import java.util.LinkedList;
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    class WordViewHolder extends RecyclerView.ViewHolder
+            {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+
+
+    }
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+    public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+        // Inflate an item view.
+        View mItemView = mInflater.inflate(
+                R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
 
+
+    @Override
+    public void onBindViewHolder(WordListAdapter.WordViewHolder holder,
+                                 int position) {
+        // Retrieve the data for that position.
+        String mCurrent = mWordList.get(position);
+        // Add the data to the view holder.
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mWordList.size();
     }
-
-    public class WordViewHolder extends RecyclerView.ViewHolder{
-        public final TextView wordItemView;
-        final WordListAdapter mAdapter;
-
-        public WordViewHolder(@NonNull View itemView, TextView wordItemView, WordListAdapter mAdapter) {
-            super(itemView);
-            this.wordItemView = wordItemView;
-            this.mAdapter = mAdapter;
-        }
-    }
-
 }
-
