40a50faf7fe367ff2433dab38f80bec2d2b929bf
AdrianPajaresPeso <dam08.2020.jesuitas@gmail.com>
2021-02-19 02:58:52 +0100

    added some logic to exercise

--- app/src/main/java/com/example/a33/model/Word.java
+++ app/src/main/java/com/example/a33/model/Word.java
@@ -1,4 +1,4 @@
-package com.example.a33;
+package com.example.a33.model;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
--- app/src/main/java/com/example/a33/model/WordDao.java
+++ app/src/main/java/com/example/a33/model/WordDao.java
@@ -1,4 +1,21 @@
 package com.example.a33.model;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface WordDao {
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>>getAllWords();
 }
