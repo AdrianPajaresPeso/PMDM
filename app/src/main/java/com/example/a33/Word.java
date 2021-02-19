47a35e73d8f0d856940dfb1a29654d786ce6a63b
AdrianPajaresPeso <dam08.2020.jesuitas@gmail.com>
2021-02-19 01:39:35 +0100

    empezando la actividad

---
+++ app/src/main/java/com/example/a33/Word.java
@@ -0,0 +1,8 @@
+package com.example.a33;
+
+@Entity
+public class Word {
+    private String mWord;
+    public Word(String word){this.mWord = word;}
+    public String getWord(){return this.mWord;}
+}
